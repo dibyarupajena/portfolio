728d0f372bd71fbe5a6a0d37c1c888bc33cc6abb
dibyarupajena <dibyathecool.jena@gmail.com>
2025-04-12 20:20:24 +0530

    reordered projects 2

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export default function Home() {
     <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
       <h1>👋 Hi, I’m Dibyarupa Jena</h1>
       <p>I’m a full-stack developer. Here’s some of my latest projects:</p>
-      <h3>🛒 <a href="https://ai-instagram-app.vercel.app/" target="_blank">AI Instagram Website</a></h3>
+      <h3>🛒 <a href="https://ai-instagram-app.vercel.app/" target="_blank">Mindful Scrolling Website</a></h3>
       <h3>🛒 <a href="https://vegmazona.vercel.app/" target="_blank">E-commerce Website</a></h3>
       <p>More projects and portfolio updates coming soon!</p>
     </div>
